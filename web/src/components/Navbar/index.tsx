3388e20c37ca33e0c6f1b0921cce51c0f04c069d
tnando <>
2021-01-28 17:44:35 -0500

    Added Logo in Navbar

--- web/src/components/Navbar/index.tsx
+++ web/src/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import FTCLogo from './FTCLogo.png';
 
 class NavbarComponent extends React.Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
+        <img src={FTCLogo} className="FTC-logo" alt="ftclogo" />
         <Navbar.Brand>FTC Competition Database</Navbar.Brand>
       </Navbar>
     );
--- web/src/pages/HomePage/index.tsx
+++ web/src/pages/HomePage/index.tsx
@@ -24,12 +24,12 @@ class HomePage extends React.Component<HomePageProps, HomePageState> {
 
         return (
             <Container>
-            <Container className={styles.welcomeMsg}><h1>Welcome, User! </h1></Container>
+            <Row className={styles.welcomeMsg}><h1>Welcome, User! </h1></Row>
             <Row style={{justifyContent: 'center'}}>
             
             {/* View Matches Card */}
 
-            <Col>
+            <Col >
             <Card style={{ width: '18rem' ,height: '20rem' ,justifyContent: 'center'}}>
                 <Card.Body>
                     <Card.Title className = {styles.cardStyle}>View Matches</Card.Title>
