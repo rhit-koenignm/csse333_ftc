d3860fe63815a5c3c15c2b413b017c3cce98f095
Nick von Bulow <nick@nvonbulow.com>
2021-01-29 00:54:48 -1000

    Added update team API endpoint

--- api/src/services/database/repositories/teams/TeamsRepository.ts
+++ api/src/services/database/repositories/teams/TeamsRepository.ts
@@ -2,12 +2,23 @@ import { AppDatabase } from '../../database';
 import { sql } from '../../util';
 import { Team } from '../../../../models/Team';
 
-const findAllQuery = sql('teams/findAll.sql')
+const findAllQuery = sql('teams/findAll.sql');
+const updateQuery = sql('teams/update.sql');
 
 export class TeamsRepository {
     constructor(private _db: AppDatabase) {}
 
     public async findAll() : Promise<Team[]> {
         return this._db.any(findAllQuery);
+    }
+
+    public async update(id: string, fields: Partial<Team>): Promise<number> {
+        let { result } = await this._db.one<{ result:number }>(updateQuery, {
+            team_id: id,
+            team_number: fields.team_number,
+            team_name: fields.team_name,
+        });
+
+        return result;
     }
 }
