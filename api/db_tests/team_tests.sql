2d5eb0bf47a96a86f8a2f2e31c2da1da49a927db
rhit-koenignm <70714241+rhit-koenignm@users.noreply.github.com>
2021-02-01 12:06:43 -0500

    trying to debug create_team

---
+++ api/db_tests/team_tests.sql
@@ -0,0 +1,11 @@
+/*
+ * A test script for our team stored procedures, create update and delete.
+ * Uncomment the queries one by one to see the functionality of each.
+ *
+ * Written by: Natalie Koenig
+ */
+
+--This first test will create two teams and show the result.--
+SELECT create_team(7351, 'Dynamic Signals');
+select * from team
+
